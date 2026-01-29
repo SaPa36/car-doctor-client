1247e40297d099549d07cdd1ca560e0b07b6942c
SaPa36 <nursapa2001@gmail.com>
2026-01-29 18:03:35 +0600

    added banner design

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -12,6 +12,16 @@ const Banner = () => {
                 <img
                     src={img1}
                     className="w-full" />
+                <div className="absolute   bottom-0 align-center flex flex-col w-1/2 
+                bg-[linear-gradient(90deg,rgba(21,21,21,1)_0%,rgba(21,21,21,0)_100%)] h-full justify-end">
+                    <h2 className="text-6xl ml-10">Affordable Price for Car Services</h2>
+                    <p className='ml-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quibusdam maiores 
+                        nesciunt quasi delectus eos fuga laboriosam inventore excepturi.</p>
+                        <div className='mt-5 mb-10 ml-10'>
+                            <button className="btn btn-success mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-secondary">Latest Project</button>
+                        </div>
+                </div>
                 <div className="absolute left-5 right-5 bottom-0 flex -translate-y-1/2 transform justify-end">
                     <a href="#slide4" className="btn btn-circle mr-3">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
