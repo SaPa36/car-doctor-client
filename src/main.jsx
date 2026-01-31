402db3a91749bc6592c6847a9b695a48e5f2535b
Sapa <nursapa2001@gmail.com>
2026-01-31 15:17:04 +0600

    stup authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
 
-
-
-
-createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
     <StrictMode>
-      <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
     </StrictMode>
   </div>
-)
+);
